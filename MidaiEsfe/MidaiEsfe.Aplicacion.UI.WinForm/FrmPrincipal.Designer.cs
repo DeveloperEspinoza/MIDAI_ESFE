2c747c0214bbdecfdf8f490988e0c8c4fe0a0c65
Piliaespinoza27 <61468865+Piliaespinoza27@users.noreply.github.com>
2020-08-04 19:51:05 -0600

    commit

--- MidaiEsfe/MidaiEsfe.Aplicacion.UI.WinForm/FrmPrincipal.Designer.cs
+++ MidaiEsfe/MidaiEsfe.Aplicacion.UI.WinForm/FrmPrincipal.Designer.cs
@@ -28,13 +28,9 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmPrincipal));
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
             this.archivoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.cerrarSeciónToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
             this.agregarTipoDePersonaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.agregarPersonaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.agregarModuloToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
@@ -50,8 +46,9 @@
             this.modificarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.notasToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.eliminarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
             this.menuStrip1.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // menuStrip1
@@ -63,7 +60,7 @@
             this.eliminarToolStripMenuItem});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(763, 24);
+            this.menuStrip1.Size = new System.Drawing.Size(653, 24);
             this.menuStrip1.TabIndex = 0;
             this.menuStrip1.Text = "menuStrip1";
             // 
@@ -86,38 +83,6 @@
             this.cerrarSeciónToolStripMenuItem.Name = "cerrarSeciónToolStripMenuItem";
             this.cerrarSeciónToolStripMenuItem.Size = new System.Drawing.Size(242, 22);
             this.cerrarSeciónToolStripMenuItem.Text = "Cerrar Seción";
-            // 
-            // pictureBox1
-            // 
-            this.pictureBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.BackgroundImage")));
-            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.pictureBox1.Location = new System.Drawing.Point(231, 109);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(289, 156);
-            this.pictureBox1.TabIndex = 0;
-            this.pictureBox1.TabStop = false;
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Arial Rounded MT Bold", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.ForeColor = System.Drawing.SystemColors.HotTrack;
-            this.label1.Location = new System.Drawing.Point(266, 44);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(201, 37);
-            this.label1.TabIndex = 1;
-            this.label1.Text = "MIDAI ESFE";
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Arial Rounded MT Bold", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.ForeColor = System.Drawing.SystemColors.HotTrack;
-            this.label2.Location = new System.Drawing.Point(180, 306);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(420, 32);
-            this.label2.TabIndex = 2;
-            this.label2.Text = "Aplicación de control de notas.";
             // 
             // agregarTipoDePersonaToolStripMenuItem
             // 
@@ -211,7 +176,7 @@
             // notasToolStripMenuItem
             // 
             this.notasToolStripMenuItem.Name = "notasToolStripMenuItem";
-            this.notasToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.notasToolStripMenuItem.Size = new System.Drawing.Size(105, 22);
             this.notasToolStripMenuItem.Text = "Notas";
             // 
             // eliminarToolStripMenuItem
@@ -220,14 +185,35 @@
             this.eliminarToolStripMenuItem.Size = new System.Drawing.Size(62, 20);
             this.eliminarToolStripMenuItem.Text = "Eliminar";
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Century", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.label1.Location = new System.Drawing.Point(214, 46);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(270, 66);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "BIENVENIDOS A \r\nMIDAI ESFE";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Century", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.label2.Location = new System.Drawing.Point(124, 346);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(438, 33);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Aplicación de control de notas.";
+            // 
             // FrmPrincipal
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(763, 401);
+            this.ClientSize = new System.Drawing.Size(653, 418);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.menuStrip1);
             this.MainMenuStrip = this.menuStrip1;
             this.Name = "FrmPrincipal";
@@ -235,7 +221,6 @@
             this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
             this.menuStrip1.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -246,7 +231,6 @@
         private System.Windows.Forms.MenuStrip menuStrip1;
         private System.Windows.Forms.ToolStripMenuItem archivoToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem cerrarSeciónToolStripMenuItem;
-        private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.ToolStripMenuItem agregarTipoDePersonaToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem agregarPersonaToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem agregarModuloToolStripMenuItem;
